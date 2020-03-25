80fa8651a5ef52843d7f4ee492f6ace5b2d7dd80
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-25 15:00:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,14 @@
 # Takes in a string and reverses each word
 def reverse_each_word(input_string)
   # First, try it with the .each method
+
   #my_return_array = []
   #input_string.split(" ").each do |word|
   #  my_return_array << word.reverse
   #end
   #my_return_array.join(" ")
 
-  # now, try it with .collect method
+  # Now, try it with .collect method
+  
   input_string.split(" ").collect {|word| word.reverse}.join(" ")
 end
