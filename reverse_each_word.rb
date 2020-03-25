5725022deb6648e91c1be6c816fa435f36c6ab03
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-25 14:58:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 # takes in a string and reverses each word
 def reverse_each_word(input_string)
-  my_return_string = ""
+  my_return_array = []
   input_string.split(" ").each do |word|
-    my_return_string << word.reverse << " "
+    my_return_string << word.reverse
   end
-  my_return_string
+  my_return_array.join(" ")
 end
